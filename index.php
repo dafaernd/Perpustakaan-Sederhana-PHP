973da8674a8b16da45276cbe0918c18b30beeefb
dafaernd <2210631170115@student.unsika.ac.id>
2024-05-27 10:59:16 +0700

    progress kedua

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aplikasi Perpustakaan</title>
+    <title>Aplikasi Perpustakaan v.2</title>
 </head>
 <body>
     <h1>Aplikasi Perpustakaan</h1>
